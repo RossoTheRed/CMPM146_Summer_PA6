02d561e9c991e0bf1516fbad62f833061a3b25f6
RossoTheRed <4mylittlesqueaky@gmail.com>
2026-08-01 12:06:48 -0700

    Basic implementation for the model

--- src/models/basic_model.py
+++ src/models/basic_model.py
@@ -7,9 +7,28 @@ class BasicModel(Model):
     def _define_model(self, input_shape, categories_count):
         # Your code goes here
         # you have to initialize self.model to a keras model
-        pass
+        self.model = Sequential([
+            layers.Input(shape=input_shape),
+            layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+            layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+            layers.Conv2D(128, kernel_size=(3, 3), activation="relu"),
+            layers.GlobalAveragePooling2D(),
+            layers.Dropout(0.5),
+            layers.Dense(categories_count, activation="softmax"),
+            # layers.Input(shape=input_shape),
+            # layers.Flatten(),
+            # layers.Dense(categories_count, activation="relu"),
+            # layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+            # layers.MaxPooling2D(pool_size=(3, 3)),
+        ])
     
     def _compile_model(self):
         # Your code goes here
         # you have to compile the keras model, similar to the example in the writeup
-        pass
+        self.model.compile(
+            optimizer=RMSprop(learning_rate=0.001),
+            loss="categorical_crossentropy",
+            metrics=["accuracy"]
+        )
